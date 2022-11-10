03cb38e3b0ceaa5a9ee299830b3b2f7e5394e861
Bean791 <bean90917@gmail.com>
2022-11-10 17:33:03 +0700

    gini dulu

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-import main from './src/script/view/main.js';
+import './styles/style.css';
+import './script/component/app-bar.js';
+import main from './script/view/main.js';
 document.addEventListener('DOMContentLoaded', main);
